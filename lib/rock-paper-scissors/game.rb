b36e144bc45d73565521ee94906ff7522d759032
Brady Bryan <jbraden.bryan@gmail.com>
2014-04-01 16:04:29 -0500

    adds game logic

--- lib/rock-paper-scissors/game.rb
+++ lib/rock-paper-scissors/game.rb
@@ -1,8 +1,8 @@
 module RPS
 	class Game
-		
+
 		attr_reader :id
-		attr_accessor :p1_move, :p2_move
+		attr_accessor :p1_move, :p2_move, :winner_id
 		@@id = 0
 
 		def initialize(match_id)
@@ -11,6 +11,33 @@ module RPS
 			@id = @@id
 			@p1_move = nil
 			@p2_move = nil
+			@winner_id = nil
+		end
+
+		def play(move)
+			if @p1_move == nil
+				@p1_move = move
+			else
+				@p2_move = move
+			end
+
+			if @p2_move != nil
+				if @p1_move == @p2_move
+					@winner_id = 0
+				elsif @p1_move == "rock" && @p2_move == "scissors"
+					@winner_id = 1
+				elsif @p1_move == "rock" && @p2_move == "paper"
+					@winner_id = 2
+				elsif @p1_move == "paper" && @p2_move == "scissors"
+					@winner_id = 2
+				elsif @p1_move == "paper" && @p2_move == "rock"
+					@winner_id = 1
+				elsif @p1_move == "scissors" && @p2_move == "paper"
+					@winner_id = 1
+				else
+					@winner_id = 2
+				end
+			end
 		end
 	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,5 +13,38 @@ module RPS
 
 			expect(new_game.id).to_not eq(newer_game.id)
 		end
+
+		it "lets user1 throw rock paper or scissors" do
+			new_game = Game.new(1)
+			new_game.play("rock")
+			expect(new_game.p1_move).to eq("rock")
+			expect(new_game.p2_move).to eq(nil)
+
+		end
+
+		it "takes user2's move after user1's move has been played" do
+			new_game = Game.new(1)
+			new_game.play("rock")
+			expect(new_game.p2_move).to eq(nil)
+			new_game.play("rock")
+			expect(new_game.p2_move).to eq("rock")
+		end
+
+		it "returns a winner after both players move" do
+			new_game = Game.new(1)
+			new_game.play("rock")
+			expect(new_game.winner_id).to eq(nil)
+			new_game.play("paper")
+			expect(new_game.winner_id).to eq(2)
+		end
+
+		it "returns '0' as winner_id after a tie game" do
+			new_game = Game.new(1)
+			new_game.play("rock")
+			new_game.play("rock")
+			expect(new_game.winner_id).to eq(0)
+		end
+
+
 	end #end 'describe RPS::Game'
 end #end module
