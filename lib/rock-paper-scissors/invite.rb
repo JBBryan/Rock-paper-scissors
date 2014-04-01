aaf934862ece52a700fd17ed50fa8d657909faaa
Antonio Felguerez <agfelgue@gmail.com>
2014-04-01 14:22:10 -0500

    Added a unique ID for each invitation instance and a test to check

--- lib/rock-paper-scissors/invite.rb
+++ lib/rock-paper-scissors/invite.rb
@@ -1,10 +1,14 @@
 module RPS
-	attr_reader :host_id, :guest_id
 
 	class Invitation
+		attr_reader :host_id, :guest_id, :id
+		@@id = 0
+
 		def initialize(host_id, guest_id)
 			@host_id = host_id
 			@guest_id = guest_id
+			@@id += 1
+			@id = @@id
 		end
 	end
 end
--- spec/invite_spec.rb
+++ spec/invite_spec.rb
@@ -6,5 +6,12 @@ module RPS
 			invite = Invitation.new(1, 2)
 			expect(invite).to be_an(Invitation)
 		end
+
+		it "receives a unique invitation ID" do
+			invite1 = Invitation.new(1, 2)
+			invite2 = Invitation.new(1, 3)
+
+			expect(invite1.id).to_not eq(invite2.id)
+		end
 	end
 end
