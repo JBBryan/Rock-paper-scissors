8a6e570e3a208b0a2f2fc9759bedae558f5d17cb
Brady Bryan <jbraden.bryan@gmail.com>
2014-04-01 14:34:28 -0500

    adds basic database class

--- lib/rock-paper-scissors/database.rb
+++ lib/rock-paper-scissors/database.rb
@@ -1,7 +1,52 @@
-# database.rb
+module RPS
+  class Database
 
+    def initialize
+      @users = {}
+      @matches = {}
+      @games = {}
+      @invites = {}
 
+    def add_user(name)
+      user = User.new(name)
+      @users[user.id] = user
+      return user
+    end
+
+    def get_user(user_id)
+      return @users[user_id]
+    end
+
+    def add_match(user1_id, user2_id)
+      match = Match.new(user1_id, user2_id)
+      @matches[match.id] = match
+      return match
+    end
+
+    def get_match(match_id)
+      return @matches[match_id]
+    end
+
+    def add_invitation(host_id, guest_id)
+      invite = Invitation.new(host_id, guest_id)
+      @invites[invite.id] = invite
+      return invite
+    end
+
+    def get_invite(invite_id)
+      return @invites[invite_id]
+    end
+
+    def add_game(match_id)
+      new_game = Game.new(match_id)
+      @games[new_game.id] = new_game
+    end
+
+    def get_game(game_id)
+      return @games[game_id]
+    end
 
   def self.db
     @__db_instance ||= Database.new
   end
+end
---
+++ spec/database_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+module RPS
+
+  describe "Database" do
+    before do
+      @db = RPS.db
+    end
+
+    it "can create a new user" do
+      user_1 = @db.add_user("Brady")
+      user_2 = @db.add_user("Tony")
+      user_3 = @db.add_user("Ronak")
+      expect(@db.get_user(user_1.id).name).to eq("Brady")
+      expect(@db.get_user(user_3.id).name).to eq("Ronak")
+      expect(@db.get_user(user_2.id).name).to eq("Tony")
+    end
+
+    it "can create a match" do
+      user_1 = @db.add_user("Tony")
+      user_2 = @db.add_user("Ronak")
+      match_1 = @db.add_match(user_1.id, user_2.id)
+      expect(@db.get_match(match_1.id)).to be_a(Match)
+    end
+
+    it "can create an invitation" do
+      user_1 = @db.add_user("Brady")
+      user_2 = @db.add_user("Tony")
+      invite = @db.add_invitation(user_1.id, user_2.id)
+      expect(@db.get_invite(invite.id)).to be_a(Invitation)
+      expect(@db.get_invite(invite.id).host_id).to eq(user_1.id)
+    end
+
+    it "can create a game" do
+      user_1 = @db.add_user("Tony")
+      user_2 = @db.add_user("Ronak")
+      match_1 = @db.add_match(user_1.id, user_2.id)
+      game_1 = @db.add_game(match_1.id)
+      expect(@db.get_game(game_1.id)).to be_a(Game)
+    end
+
+  end
+end
+
+
