177e758b3a465d5737bc40f9946e1739bc4c65c0
Antonio Felguerez <agfelgue@gmail.com>
2014-04-01 14:11:20 -0500

    Adds Game class and RSpec tests:
    1. Game class exists
    2. Game class receives a unique game ID
    
    Game class takes two player IDs and a match ID

---
+++ lib/rock-paper-scissors/game.rb
@@ -0,0 +1,18 @@
+module RPS
+	class Game
+		
+		attr_reader :id
+		attr_accessor :p1_move, :p2_move
+		@@id = 0
+
+		def initialize(p1_id, p2_id, match_id)
+			@p1_id = p1_id
+			@p2_id = p2_id
+			@match_id = match_id
+			@@id +=1
+			@id = @@id
+			@p1_move = nil
+			@p2_move = nil
+		end
+	end
+end
