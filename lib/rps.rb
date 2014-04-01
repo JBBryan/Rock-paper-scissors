efe969e206bb772460b0e3a01788d70ebf8f7619
Brady Bryan <jbraden.bryan@gmail.com>
2014-04-01 13:20:42 -0500

    updated with database class

---
+++ lib/rps.rb
@@ -0,0 +1,9 @@
+module RPS
+end
+
+require_relative 'rock-paper-scissors/game.rb'
+require_relative 'rock-paper-scissors/invite.rb'
+require_relative 'rock-paper-scissors/match.rb'
+require_relative 'rock-paper-scissors/user.rb'
+require_relative 'rock-paper-scissors/database.rb'
+require 'pry-debugger'
